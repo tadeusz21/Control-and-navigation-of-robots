ff18c4582d95c26a520fd229d83020a030b834db
Tadeusz Chmielik <t.j.chmielik@gmail.com>
2024-11-07 00:00:53 +0100

    lab2_zad10

--- lab1/hello_moveit/src/one_grasp.cpp
+++ lab1/hello_moveit/src/one_grasp.cpp
@@ -65,7 +65,20 @@
 #include <rclcpp/rclcpp.hpp>
 #include <geometry_msgs/msg/pose.hpp>
 #include <gazebo_msgs/srv/get_entity_state.hpp>
+#include <tf2_eigen/tf2_eigen.h>
 
+geometry_msgs::msg::Pose toPoseMsg(const Eigen::Isometry3d& transform) {
+    geometry_msgs::msg::Pose pose_msg;
+    pose_msg.position.x = transform.translation().x();
+    pose_msg.position.y = transform.translation().y();
+    pose_msg.position.z = transform.translation().z();
+    Eigen::Quaterniond q(transform.rotation());
+    pose_msg.orientation.x = q.x();
+    pose_msg.orientation.y = q.y();
+    pose_msg.orientation.z = q.z();
+    pose_msg.orientation.w = q.w();
+    return pose_msg;
+}
 int main(int argc, char* argv[])
 {
   // Initialize ROS and create the Node
@@ -121,36 +134,146 @@ int main(int argc, char* argv[])
   Eigen::Isometry3d T_BC = Eigen::Translation3d(0.6129532489572659, 0.12172421971793858, 0.33512117754742166) *
   Eigen::Quaterniond(-2.121740371074175e-05, -0.0003902260631371974, 0.014435026569939098, 0.999895733204901).normalized();
 
+  Eigen::Isometry3d T_GC = Eigen::Translation3d(-0.3, 0.0, 0.0) *
+  Eigen::Quaterniond(0.0, 0.0, 0.0, 1.0).normalized();
+
+  Eigen::Isometry3d T_spin = Eigen::Translation3d(0.0, 0.0, 0.0) *
+  Eigen::Quaterniond(0.0, 0.0, 0.0, 1.0).normalized();
+
   Eigen::Isometry3d T_BE = T_BF * T_FE;
   Eigen::Isometry3d T_CE  = T_BC.inverse() * T_BE;
 
-  Eigen::Isometry3d end_effector_pose = cube_pose * T_CE;
+  Eigen::Isometry3d end_effector_pose = cube_pose * T_CE * T_spin;
+  Eigen::Isometry3d pre_grip_pose = end_effector_pose * T_GC * T_spin;
+
+
 
   moveit_visual_tools.publishAxis(end_effector_pose, 0.5, 0.01, "end_effector");  // Adjust size as needed
+  moveit_visual_tools.publishAxis(pre_grip_pose, 0.1, 0.05, "pre_grisp");
   moveit_visual_tools.trigger();
 
 
+using moveit::planning_interface::MoveGroupInterface;
+    MoveGroupInterface move_group_interface(node, "arm_torso"); 
+
+  move_group_interface.setMaxVelocityScalingFactor(1.0); 
+  move_group_interface.setMaxAccelerationScalingFactor(1.0);
+    
+
+
+  auto move_group_interface_gripper = MoveGroupInterface(node, "gripper");
+  // auto move_group_interface_arm = MoveGroupInterface(node, "arm");
+
+//  move_group_interface.setJointValueTarget(std::vector<double>({0.187, 100.0, 41.0, 0.0, 63.0, 0.0, 0.0, -78.0})); 
+//  moveit::planning_interface::MoveGroupInterface::Plan plan_0;
+//   auto const success_0 = static_cast<bool>(move_group_interface.plan(plan_0));
+
+  // move_group_interface_arm.setMaxVelocityScalingFactor(1.0); 
+  // move_group_interface_arm.setMaxAccelerationScalingFactor(1.0);
+
+
+
+//   if (success_0) {
+//   move_group_interface.execute(plan_0);
+// } else {
+//   RCLCPP_ERROR(logger, "Planning failed!");
+// }
+
+
+  move_group_interface_gripper.setJointValueTarget(std::vector<double>({0.05, 0.05}));
+  moveit::planning_interface::MoveGroupInterface::Plan plan_grip_1;
+  auto const success_grip_1 = static_cast<bool>(move_group_interface_gripper.plan(plan_grip_1));
 
   
 
 
-  // geometry_msgs::Pose pose;
 
-  //   // Ustaw pozycję
-  //   pose.position.x = 0.0;
-  //   pose.position.y = 0.0;
-  //   pose.position.z = 0.046;
+  if (success_grip_1) {
+  move_group_interface.execute(plan_grip_1);
+} else {
+  RCLCPP_ERROR(logger, "Planning failed!");
+}
 
-  //   // Ustaw orientację jako kwaternion
-  //   // W ROS kwaternion jest w formacie (x, y, z, w)
-  //   pose.orientation.x = 0.5;
-  //   pose.orientation.y = 0.5;
-  //   pose.orientation.z = 0.5;
-  //   pose.orientation.w = -0.5;
 
- 
-  // Isometry3d p = Translation3d(0.62, 0.10, 0.33) *
-  // Quaterniond(0.0, 0.0, 0.0, 1.0).normalized();
+
+// Skonwertuj wyliczoną pozycję na geometry_msgs::msg::Pose
+geometry_msgs::msg::Pose pre_target_pose = toPoseMsg(pre_grip_pose);
+
+// Ustaw cel pozycji
+move_group_interface.setPoseTarget(pre_target_pose);
+
+// Stwórz plan do tej pozycji
+auto const [success, plan] = [&move_group_interface] {
+    moveit::planning_interface::MoveGroupInterface::Plan msg;
+    auto const ok = static_cast<bool>(move_group_interface.plan(msg));
+    return std::make_pair(ok, msg);
+}();
+
+// Wykonaj plan
+if (success) {
+    move_group_interface.execute(plan);
+} else {
+    RCLCPP_ERROR(logger, "Planning failed!");
+}
+
+
+
+geometry_msgs::msg::Pose grip_target_pose = toPoseMsg(end_effector_pose);
+
+// Ustaw cel pozycji
+move_group_interface.setPoseTarget(grip_target_pose);
+
+// Stwórz plan do tej pozycji
+auto const [success_2, plan_2] = [&move_group_interface] {
+    moveit::planning_interface::MoveGroupInterface::Plan msg_2;
+    auto const ok_2 = static_cast<bool>(move_group_interface.plan(msg_2));
+    return std::make_pair(ok_2, msg_2);
+}();
+
+// Wykonaj plan
+if (success_2) {
+    move_group_interface.execute(plan_2);
+} else {
+    RCLCPP_ERROR(logger, "Planning failed!");
+}
+
+
+move_group_interface_gripper.setJointValueTarget(std::vector<double>({0.033, 0.033}));
+  moveit::planning_interface::MoveGroupInterface::Plan plan_grip_2;
+  auto const success_grip_2 = static_cast<bool>(move_group_interface_gripper.plan(plan_grip_2));
+
+  move_group_interface.setMaxVelocityScalingFactor(1.0); 
+  move_group_interface.setMaxAccelerationScalingFactor(1.0);
+
+  move_group_interface_gripper.setMaxVelocityScalingFactor(1.0); 
+  move_group_interface_gripper.setMaxAccelerationScalingFactor(1.0);
+
+
+
+  if (success_grip_2) {
+  move_group_interface.execute(plan_grip_2);
+} else {
+  RCLCPP_ERROR(logger, "Planning failed!");
+}
+
+
+// Ustaw cel pozycji
+move_group_interface.setPoseTarget(pre_target_pose);
+
+// Stwórz plan do tej pozycji
+auto const [success_3, plan_3] = [&move_group_interface] {
+    moveit::planning_interface::MoveGroupInterface::Plan msg_3;
+    auto const ok_3 = static_cast<bool>(move_group_interface.plan(msg_3));
+    return std::make_pair(ok_3, msg_3);
+}();
+
+// Wykonaj plan
+if (success_3) {
+    move_group_interface.execute(plan_3);
+} else {
+    RCLCPP_ERROR(logger, "Planning failed!");
+}
+
 
   // Shutdown ROS
   rclcpp::spin(node);
@@ -164,58 +287,4 @@ int main(int argc, char* argv[])
 
 
 
-  // // Set a target Pose
-  // auto const target_pose = [] {
-  //   geometry_msgs::msg::Pose msg;
-  //   msg.orientation.w = 1.0;
-  //   msg.position.x = 0.4;
-  //   msg.position.y = -0.2;
-  //   msg.position.z = 0.5;
-  //   return msg;
-  // }();
-  // move_group_interface.setPoseTarget(target_pose);
-
-  // // Create a plan to that target pose
-  // prompt("Press 'next' in the RvizVisualToolsGui window to plan");
-  // draw_title("Planning");
-  // moveit_visual_tools.trigger();
-  // auto const [success, plan] = [&move_group_interface] {
-  //   moveit::planning_interface::MoveGroupInterface::Plan msg;
-  //   auto const ok = static_cast<bool>(move_group_interface.plan(msg));
-  //   return std::make_pair(ok, msg);
-  // }();
-
-  // // Execute the plan
-  // if (success)
-  // {
-  //   draw_trajectory_tool_path(plan.trajectory);
-  //   moveit_visual_tools.trigger();
-  //   prompt("Press 'next' in the RvizVisualToolsGui window to execute");
-  //   draw_title("Executing");
-  //   moveit_visual_tools.trigger();
-  //   move_group_interface.execute(plan);
-  // }
-  // else
-  // {
-  //   draw_title("Planning Failed!");
-  //   moveit_visual_tools.trigger();
-  //   RCLCPP_ERROR(logger, "Planning failed!");
-  // }
-
-  // auto planning_frame = move_group_interface.getPlanningFrame();
-  // auto end_effector_link = move_group_interface.getEndEffectorLink();
-
-  // RCLCPP_INFO(logger, "układ bazowy (B): %s", planning_frame.c_str());
-  // RCLCPP_INFO(logger, "układ końcówki (E): %s", end_effector_link.c_str());
-
-
-
-
-
-
-
-  // Shutdown ROS
-//   rclcpp::shutdown();
-//   spinner.join();
-//   return 0;
-// }
+  
